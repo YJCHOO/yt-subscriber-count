03f17de4b588ed93fcbdd0a70611eb93e1f09c75
Choo Ying Juan <33310617+YJCHOO@users.noreply.github.com>
2021-09-08 15:11:59 +0800

    add invertDisplay function.
    To avoid OLED pixel burn in.

--- yt-subscriber-count.ino
+++ yt-subscriber-count.ino
@@ -1,9 +1,8 @@
-
 #include <ESP8266WiFi.h>
 #include <WiFiClientSecure.h>
 #include <Wire.h>
-#include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
+#include <Adafruit_GFX.h>
 #include <YoutubeApi.h>
 #include <ArduinoJson.h>
 
@@ -11,7 +10,7 @@
 #define SCREEN_WIDTH 128
 #define SCREEN_HEIGHT 64
 
-//
+//init OLED Driver
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
 
 //WiFi credential
@@ -24,12 +23,15 @@ char password[] = "<Your Wi-Fi Password>";
 //YouTube Channel ID
 #define YT_CHANNEL_ID "<Your YouTube Channel ID>"
 
+//Use this client to connect a secure server.
 WiFiClientSecure client;
 YoutubeApi api(YT_DATA_API_KEY, client);
 
 unsigned long interval = 60*60*1000UL;
+bool isInvert = false;
 
 // 'Youtube Logo', 32x32px
+// Byte Array
 const unsigned char myBitmap [] PROGMEM = {
   0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 
   0x00, 0x00, 0x00, 0x00, 0x1f, 0xff, 0xff, 0xf8, 0x3f, 0xff, 0xff, 0xfc, 0x7f, 0xff, 0xff, 0xfe, 
@@ -41,8 +43,6 @@ const unsigned char myBitmap [] PROGMEM = {
   0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
 };
 
-long subs = 0;
-
 void setup() {
   Serial.begin(115200);
   Wire.begin(0, 2); 
@@ -53,12 +53,13 @@ void setup() {
     for(;;);
   }
 
-  //clear the initail Adafruit Industries Logo
+  //clear the initial Adafruit Industries Logo
   display.clearDisplay();
   display.display();
 
-  initDisplay();
-
+  display.setCursor(0, 0);
+  display.setTextColor(WHITE);
+  
   display.print("Connecting WiFi: ");
   display.print(ssid);
   display.display();
@@ -71,8 +72,8 @@ void setup() {
   }
   
   display.clearDisplay();
-  initDisplay();
-  
+  display.setCursor(0, 0);
+
   display.println("WiFi connected.");
   display.println("IP address: ");
   IPAddress ip = WiFi.localIP();
@@ -80,13 +81,13 @@ void setup() {
   display.display();
 }
 
-void loop() {  
+void loop() {
+  display.invertDisplay(isInvert);
   if(api.getChannelStatistics(YT_CHANNEL_ID)) {
     display.clearDisplay();
     display.drawBitmap(5, 15, myBitmap, 32, 32, WHITE);
 
     display.setCursor(70, 23);
-    display.setTextColor(WHITE);
     display.println(api.channelStats.subscriberCount);
     
     display.setCursor(45, 33);
@@ -95,10 +96,5 @@ void loop() {
     display.display();
   }
   delay(interval);
-}
-
-void initDisplay () {
-  display.setTextSize(1);
-  display.setTextColor(WHITE);
-  display.setCursor(0, 0);
+  isInvert = !isInvert;
 }
